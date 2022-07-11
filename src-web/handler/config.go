d9085186f78a48526a19614a4d9c8497dcdf28a8
yz-hl <yz_hl@oi-liu.com>
2022-07-11 12:44:34 +0800

    POST Success! No unit tests are added...

--- src-web/handler/config.go
+++ src-web/handler/config.go
@@ -3,12 +3,25 @@ package handler
 import (
 	"fmt"
 	"github.com/gin-gonic/gin"
+	"log"
+	"os"
 )
 
-func saveCodeforcesConfig(context *gin.Context) {
+func SaveCodeforcesConfig(context *gin.Context) {
 	apiKey := context.PostForm("apiKey")
 	apiSecret := context.PostForm("apiSecret")
 	usernameOrEmail := context.PostForm("usernameOrEmail")
 	password := context.PostForm("password")
 	fmt.Println(apiKey, apiSecret, usernameOrEmail, password)
+
+	f, err := os.Create("file.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	// remember to close the file
+	defer f.Close()
+	_, err = f.WriteString(apiKey + apiSecret + usernameOrEmail + password + "\n")
+	if err != nil {
+		log.Fatal(err)
+	}
 }
--- src-web/router/router.go
+++ src-web/router/router.go
@@ -7,7 +7,9 @@ import (
 
 func SetupRouter() *gin.Engine {
 	router := gin.Default()
+	//router.Static("/statics", "./statics")
 	router.LoadHTMLGlob("templates/*")
 	router.Any("/", handler.Index)
+	router.POST("/auth", handler.SaveCodeforcesConfig)
 	return router
 }
